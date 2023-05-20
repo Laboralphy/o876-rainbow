3af3d218cf4cc09bb8ed463b888ea87c83165047
Raphael Marandet <raphael.marandet@gmail.com>
2023-05-20 12:20:28 +0200

    support hsl

--- tests/RainbowTest.mjs
+++ tests/RainbowTest.mjs
@@ -1,11 +1,32 @@
-import Rainbow from '../src/Rainbow.js';
+import Rainbow, { Color } from '../src/Rainbow.js';
 
 describe('#parse', function() {
-    it('should parse 3 digit color code', function() {
+    it('should return rgba(0, 0, 0, 1) when parsing 3 digit color code #000', function() {
         expect(Rainbow.rgba('#000')).toBe('rgba(0, 0, 0, 1)');
     })
     it('should parse color structure', function() {
         expect(Rainbow.rgba({ r: 129, g: 66, b: 177, a: 255})).toBe('rgba(129, 66, 177, 1)');
+    })
+    it('should parse color structure', function() {
+        expect(Rainbow.rgba({ r: 129, g: 66, b: 177, a: 255})).toBe('rgba(129, 66, 177, 1)');
+    })
+    it('should parse 1', function () {
+        const x = Rainbow.parse('blue')
+        expect(x).toBeInstanceOf(Color)
+        expect(x.hex(3)).toBe('#00f')
+        expect(x.hex(6)).toBe('#0000ff')
+        expect(x.hex(8)).toBe('#0000ffff')
+        expect(x.rgba()).toBe('rgba(0, 0, 255, 1)')
+        expect(x.rgb()).toBe('rgb(0, 0, 255)')
+    })
+    it('should understand color lightgoldenrodyellow', function () {
+        // lightgoldenrodyellow #FAFAD2
+        const x = Rainbow.parse('lightgoldenrodyellow')
+        expect(x.hex(3)).toBe('#ffd')
+        expect(x.hex(6)).toBe('#fafad2')
+        expect(x.hex(8)).toBe('#fafad2ff')
+        expect(x.rgba()).toBe('rgba(250, 250, 210, 1)')
+        expect(x.rgb()).toBe('rgb(250, 250, 210)')
     })
 })
 
@@ -24,3 +45,4 @@ describe('#int32', function() {
         expect(Rainbow.int32('rgba(8, 7, 1, 1)')).toBe(0xFF010708 | 0)
     })
 })
+
